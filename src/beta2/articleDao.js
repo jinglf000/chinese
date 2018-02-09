0a471c3cb49f00e998121cd71ff01462e8ef42d8
jinglf000 <jinglongfei@jsb5.com>
2018-02-09 18:10:57 +0800

    feat(module):开始添加了DAO层的操作
    添加了mongoose

---
+++ src/beta2/articleDao.js
@@ -0,0 +1,10 @@
+/**
+ * article Dao 层，数据库操作
+ * @author jinglf000
+ * ###### Fri Feb 9 18:05:01 CST 2018
+ */
+
+const mongoose = require('mongoose');
+
+// 连接本地测试库
+mongoose.connect('mongodb://localhost/test');
